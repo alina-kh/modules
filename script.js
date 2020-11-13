fcdc1db7257d7706c9b93939990ef45e34ac5e26
Alina Khitrova <61740745+alina-kh@users.noreply.github.com>
2020-11-13 11:17:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-import calc		from './modules/calc';
+import calc	from './modules/calc';
 import cards	from './modules/cards';
 import forms	from './modules/forms';
 import modal	from './modules/modal';
 import slider	from './modules/slider';
-import tabs		from './modules/tabs';
+import tabs	from './modules/tabs';
 import timer	from './modules/timer';
 import openModal from './modules/modal';
 
